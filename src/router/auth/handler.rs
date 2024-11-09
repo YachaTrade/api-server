93e3ff33efd7ae2138598babf077de18fb8eef4f
gyu <gyu@gyuui-MacBookPro.local>
2024-11-09 17:16:43 +0900

    test cookie

--- src/router/auth/handler.rs
+++ src/router/auth/handler.rs
@@ -163,7 +163,7 @@ pub async fn auth_session(
     //추후 프론트 배포시 samesite = strict 로 변경
     //Secure 추가  SameSite=None;
     let cookie = format!(
-        "session={}; HttpOnly; Path=/; Max-Age={};",
+        "session={}; HttpOnly; Path=/; Max-Age={}",
         session_id, max_age,
     );
     info!("cookie = {:?}", cookie);
