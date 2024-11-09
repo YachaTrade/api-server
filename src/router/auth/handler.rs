0d95b2824860ffb083df10c841e128cdd57352a2
gyu <gyu@gyuui-MacBookPro.local>
2024-11-09 21:21:12 +0900

    Modify cookie secure

--- src/router/auth/handler.rs
+++ src/router/auth/handler.rs
@@ -167,7 +167,7 @@ pub async fn auth_session(
     //     session_id, max_age,
     // );
     let cookie = format!(
-        "session={}; HttpOnly; Path=/; Max-Age={};SameSite=None",
+        "session={}; HttpOnly; Secure; Path=/; Max-Age={};SameSite=None",
         session_id, max_age
     );
     info!("cookie = {:?}", cookie);
