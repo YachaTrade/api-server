7e394a10b1711bea85c696e47b89293167ec2789
gyu <gyu@gyuui-MacBookPro.local>
2024-12-16 03:46:17 +0900

    Fix : test cookie

--- src/router/auth/handler.rs
+++ src/router/auth/handler.rs
@@ -161,10 +161,10 @@ pub async fn auth_session(
     //추후 프론트 배포시 samesite = strict 로 변경
     let mut cookie = Cookie::new("session", session_id);
     cookie.set_http_only(true);
-    cookie.set_domain("api-server.nad.fun"); // 변경
+    // cookie.set_domain("nad.fun"); // 변경
     cookie.set_secure(true);
     cookie.set_path("/");
-    cookie.set_same_site(tower_cookies::cookie::SameSite::Strict);
+    // cookie.set_same_site(tower_cookies::cookie::SameSite::Lax);
     cookie.set_max_age(Duration::days(7));
 
     info!("cookie = {:?}", cookie);
