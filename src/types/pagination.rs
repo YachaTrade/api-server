b4fb619f2ddeab6704dfc3ec94d6251d9a93723b
gyu <gyu@gyuui-MacBookPro.local>
2025-01-15 16:19:58 +0900

    ADD : PaginationParams

---
+++ src/types/pagination.rs
@@ -0,0 +1,30 @@
+use serde::Deserialize;
+use utoipa::ToSchema;
+
+#[derive(Deserialize, ToSchema)]
+pub struct PaginationParams {
+    #[serde(default = "default_page")]
+    pub page: i64,
+    #[serde(default = "default_limit", deserialize_with = "validate_limit")]
+    pub limit: i64,
+}
+
+fn default_page() -> i64 {
+    1
+}
+
+fn default_limit() -> i64 {
+    10
+}
+
+fn validate_limit<'de, D>(deserializer: D) -> Result<i64, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let limit = i64::deserialize(deserializer)?;
+    if limit > 100 {
+        Err(serde::de::Error::custom("Limit must be 100 or less"))
+    } else {
+        Ok(limit)
+    }
+}
