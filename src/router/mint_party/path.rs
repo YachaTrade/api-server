be3948aa60d632fba04eb6f3d425a6cca626bb2b
gyu <gyu@gyuui-MacBookPro.local>
2025-01-15 16:22:12 +0900

    Fix : refactoring

--- src/router/mint_party/path.rs
+++ src/router/mint_party/path.rs
@@ -1,30 +1,30 @@
-#[derive(Debug)]
-pub enum MintPartyPath {
-    UpdateMintParty,
-    GetLastJoinMintParty,
-    GetMintPartyList,
-    GetMintPartyDepositList,
-    GetMintPartyBalanceList,
-}
+// #[derive(Debug)]
+// pub enum MintPartyPath {
+//     UpdateMintParty,
+//     GetLastJoinMintParty,
+//     GetMintPartyList,
+//     GetMintPartyDepositList,
+//     GetMintPartyBalanceList,
+// }
 
-impl MintPartyPath {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            MintPartyPath::UpdateMintParty => "/mint_party/update",
-            MintPartyPath::GetLastJoinMintParty => "/mint_party/last_join",
-            MintPartyPath::GetMintPartyList => "/mint_party/list",
-            MintPartyPath::GetMintPartyDepositList => "/mint_party/deposit_list", //session address check
-            MintPartyPath::GetMintPartyBalanceList => "/mint_party/balance", //session address check
-        }
-    }
-    //swager 전용 path
-    pub fn docs_str(&self) -> &'static str {
-        match self {
-            MintPartyPath::UpdateMintParty => "/mint_party/update",
-            MintPartyPath::GetLastJoinMintParty => "/mint_party/last_join",
-            MintPartyPath::GetMintPartyList => "/mint_party/list",
-            MintPartyPath::GetMintPartyDepositList => "/mint_party/deposit_list", //session address check
-            MintPartyPath::GetMintPartyBalanceList => "/mint_party/balance", //session address check
-        }
-    }
-}
+// impl MintPartyPath {
+//     pub fn as_str(&self) -> &'static str {
+//         match self {
+//             MintPartyPath::UpdateMintParty => "/mint_party/update",
+//             MintPartyPath::GetLastJoinMintParty => "/mint_party/last_join",
+//             MintPartyPath::GetMintPartyList => "/mint_party/list",
+//             MintPartyPath::GetMintPartyDepositList => "/mint_party/deposit_list", //session address check
+//             MintPartyPath::GetMintPartyBalanceList => "/mint_party/balance", //session address check
+//         }
+//     }
+//     //swager 전용 path
+//     pub fn docs_str(&self) -> &'static str {
+//         match self {
+//             MintPartyPath::UpdateMintParty => "/mint_party/update",
+//             MintPartyPath::GetLastJoinMintParty => "/mint_party/last_join",
+//             MintPartyPath::GetMintPartyList => "/mint_party/list",
+//             MintPartyPath::GetMintPartyDepositList => "/mint_party/deposit_list", //session address check
+//             MintPartyPath::GetMintPartyBalanceList => "/mint_party/balance", //session address check
+//         }
+//     }
+// }
