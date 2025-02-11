95b31ae606471f3772a2c26f8a432b9c1b0d9ed4
gyu <gyu@gyuui-MacBookPro.local>
2025-02-12 00:16:17 +0900

    Fix : token created price -> string

--- src/types/token/create_token.rs
+++ src/types/token/create_token.rs
@@ -22,7 +22,7 @@ pub struct TokenCreated {
     pub created_at: i64,
     pub market_cap: BigDecimal,     //market cap -> price * total_supply
     pub total_supply: BigDecimal,   // token table total_supply
-    pub price: BigDecimal,          // market table price
+    pub price: String,              // market table price
     pub current_amount: BigDecimal, //position table current_token_amount
     pub description: Option<String>,
 }
@@ -88,7 +88,7 @@ impl TokenCreatedController {
                 name,
                 is_listing as "is_listing!",
                 created_at,
-                COALESCE(price, 0) as "price!",
+                COALESCE(price::TEXT, '0') as "price!",
                 total_supply as "total_supply!",
                 COALESCE(price * total_supply, 0) as "market_cap!",
                 COALESCE(current_amount, 0) as "current_amount!",
