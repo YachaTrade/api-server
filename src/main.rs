d7905dc70a357b4fb285d02b4b09c69f7d7e609c
gyu <gyu@gyuui-MacBookPro.local>
2025-02-15 00:52:30 +0900

    Remove : campagin router

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use api_server::{
     cors::get_cors,
     middleware::authenticate_user,
-    router::{self, account, auth, campaign, follow, order, profile, search, thread, token, trade},
+    router::{self, account, auth,  follow, order, profile, search, thread, token, trade},
     state::AppState,
     types,
 };
