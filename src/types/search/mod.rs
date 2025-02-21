f35a5ecd48373283a984a1a3fab5fa08f872d6ca
gyu <gyu@gyuui-MacBookPro.local>
2025-02-21 23:44:02 +0900

    Refactor : search query optimize

--- src/types/search/mod.rs
+++ src/types/search/mod.rs
@@ -103,76 +103,93 @@ impl SearchController {
         let now = Utc::now().timestamp();
         let seven_days_ago = now - (7 * 24 * 60 * 60);
 
-        let account_records = sqlx::query!(
+        let account_id = sqlx::query!(
             r#"
-            SELECT 
-                a.account_id,
-                a.nickname,
-                a.image_uri,
-                a.follower_count,
-                a.following_count,
-                COALESCE(ps.total_cost, 0)::numeric AS "total_cost!: BigDecimal",
-                COALESCE(ps.total_profit, 0)::numeric AS "total_profit!: BigDecimal"
-            FROM account a
-            LEFT JOIN (
-                    SELECT 
-                        p.account_id,
-                        SUM(p.total_bought_native) AS total_cost,
-                      (SUM(p.realized_pnl) + SUM(
-                        COALESCE(
-                            CASE 
-                                WHEN p.current_token_amount = 0 THEN 0
-                                WHEN m.market_type = 'CURVE' THEN
-                                    m.virtual_native 
-                                    - (
-                                        ((m.virtual_token * m.virtual_native) 
-                                        + (m.virtual_token + p.current_token_amount) - 1)
-                                        / (m.virtual_token + p.current_token_amount)
-                                    )
-                                WHEN m.market_type = 'DEX' THEN
-                                    m.price * p.current_token_amount
-                                ELSE 0
-                            END,
-                        0)
-                    )) AS total_profit
-                    FROM position p            
-                    JOIN market m ON p.token_id = m.token_id
-                    WHERE p.created_at >= $2
-                    GROUP BY p.account_id
-                ) ps ON a.account_id = ps.account_id
-                WHERE LOWER(a.account_id) LIKE $1
-                OR LOWER(a.nickname) LIKE $1
-                ORDER BY 
-                    (
-                   COALESCE(ps.total_profit, 0)
-                    / NULLIF(COALESCE(ps.total_cost, 0), 0)
-                    ) DESC
+            SELECT account_id
+            FROM account
+            WHERE LOWER(account_id) LIKE $1
+            OR LOWER(nickname) LIKE $1
             "#,
-            search_pattern,
-            seven_days_ago,
+            search_pattern
         )
-        .fetch_all(self.db.get_read_pool())
+        .fetch_optional(self.db.get_read_pool())
         .await
-        .map_err(|e| anyhow::anyhow!(e))?;
+        .map_err(|err| anyhow::anyhow!(err))?;
 
-        let accounts_vec: Vec<SearchAccount> = account_records
-            .into_iter()
-            .map(|row| {
-                let roi_percentage = row.total_profit.clone() / row.total_cost;
-                SearchAccount {
-                    account_info: AccountInfo {
-                        account_id: row.account_id,
-                        nickname: row.nickname,
-                        image_uri: row.image_uri,
-                        follower_count: row.follower_count,
-                        following_count: row.following_count,
-                    },
-                    period: "7D".to_string(), // 여기서는 전체 기간으로 계산된 값입니다.
-                    total_profit: row.total_profit,
-                    roi_percentage,
-                }
-            })
-            .collect();
+        let accounts_vec = match account_id {
+            Some(row) => {
+                let account_records = sqlx::query!(
+                    r#"
+                    SELECT 
+                        a.account_id,
+                        a.nickname,
+                        a.image_uri,
+                        a.follower_count,
+                        a.following_count,
+                        COALESCE(ps.total_cost, 0)::numeric AS "total_cost!: BigDecimal",
+                        COALESCE(ps.total_profit, 0)::numeric AS "total_profit!: BigDecimal"
+                    FROM account a
+                    LEFT JOIN (
+                            SELECT 
+                                p.account_id,
+                                SUM(p.total_bought_native) AS total_cost,
+                              (SUM(p.realized_pnl) + SUM(
+                                COALESCE(
+                                    CASE 
+                                        WHEN p.current_token_amount = 0 THEN 0
+                                        WHEN m.market_type = 'CURVE' THEN
+                                            m.virtual_native 
+                                            - (
+                                                ((m.virtual_token * m.virtual_native) 
+                                                + (m.virtual_token + p.current_token_amount) - 1)
+                                                / (m.virtual_token + p.current_token_amount)
+                                            )
+                                        WHEN m.market_type = 'DEX' THEN
+                                            m.price * p.current_token_amount
+                                        ELSE 0
+                                    END,
+                                0)
+                            )) AS total_profit
+                            FROM position p            
+                            JOIN market m ON p.token_id = m.token_id
+                            WHERE p.created_at >= $2
+                            GROUP BY p.account_id
+                        ) ps ON a.account_id = ps.account_id
+                        WHERE a.account_id = $1
+                        ORDER BY 
+                            (
+                           COALESCE(ps.total_profit, 0)
+                            / NULLIF(COALESCE(ps.total_cost, 0), 0)
+                            ) DESC
+                    "#,
+                    row.account_id,
+                    seven_days_ago,
+                )
+                .fetch_all(self.db.get_read_pool())
+                .await
+                .map_err(|e| anyhow::anyhow!(e))?;
+
+                account_records
+                    .into_iter()
+                    .map(|row| {
+                        let roi_percentage = row.total_profit.clone() / row.total_cost;
+                        SearchAccount {
+                            account_info: AccountInfo {
+                                account_id: row.account_id,
+                                nickname: row.nickname,
+                                image_uri: row.image_uri,
+                                follower_count: row.follower_count,
+                                following_count: row.following_count,
+                            },
+                            period: "7D".to_string(), // 여기서는 전체 기간으로 계산된 값입니다.
+                            total_profit: row.total_profit,
+                            roi_percentage,
+                        }
+                    })
+                    .collect()
+            }
+            None => Vec::new(),
+        };
 
         let accounts = SearchAccountResponse {
             total_count: accounts_vec.len() as i64,
