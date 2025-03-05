e7f5ceb373607e646394f12d1df7305a38def4d5
gyu <gyu@gyuui-MacBookPro.local>
2025-03-06 02:16:15 +0900

    Fix : cache time change

--- src/config.rs
+++ src/config.rs
@@ -8,15 +8,14 @@ lazy_static! {
     pub static ref SEARCH_EXPIRATION:u64 = 30_000; //30 secons in milliseconds
     pub static ref TOKEN_EXPIRATION:u64 = 1500; //miliseconds
 
-    pub static ref TOP_POINT_EXPIRATION:u64 = 300_000; // 5 minutes in milliseconds
-    pub static ref MISSION_EXPIRATION:u64 = 1500;// 5 minutes in milliseconds
+    pub static ref TOP_POINT_EXPIRATION:u64 = 60_000; // 1 minutes in milliseconds
+    pub static ref MISSION_EXPIRATION:u64 = 60_000; // 1 minutes in milliseconds
     pub static ref ACCOUNT_POINT_EXPIRATION: u64 = 60_000; // 1 minute in milliseconds
     pub static ref REFERRAL_CODE_EXPIRATION:u64 = 10_000; // 10 seconds in milliseconds
-    pub static ref REFERRAL_CHILD_COUNT_EXPIRATION:u64 = 300_000; // 5 minutes in milliseconds
-    pub static ref INVITED_CREATE_EXPIRATION:u64 = 300_000; // 5 minutes in milliseconds
+    pub static ref REFERRAL_CHILD_COUNT_EXPIRATION:u64 = 60_000; // 1 minutes in milliseconds
+    pub static ref INVITED_CREATE_EXPIRATION:u64 = 60_000; // 1 minutes in milliseconds
     pub static ref PNL_EXPIRATION:u64 = 60_000; // 1 minute in milliseconds
     pub static ref POSITION_EXPIRATION:u64 = 60_000; // 1 minutes in milliseconds
-
-    pub static ref GET_TOKEN_RESPONSE_EXPIRATION: u64 = 600_000; // 10 minutes in milliseconds
+    pub static ref GET_TOKEN_RESPONSE_EXPIRATION: u64 = 60_000; // 1 minutes in milliseconds
 
 }
