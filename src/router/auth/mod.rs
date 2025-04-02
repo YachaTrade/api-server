e4b5a7dbd666811298787773eccb6596004b5a1c
gyu <gyu@gyuui-MacBookPro.local>
2025-04-02 23:50:17 +0900

    Feat : smart wallet session

--- src/router/auth/mod.rs
+++ src/router/auth/mod.rs
@@ -7,16 +7,16 @@ use axum::{
     Router,
 };
 use handler::{auth_delete_session, auth_nonce, auth_session};
-use path::Path;
+use path::AuthPath;
 
 use crate::{middleware::authenticate_user, state::AppState};
 
 pub fn router(app_state: AppState) -> Router<AppState> {
     Router::new()
-        .route(Path::Nonce.as_str(), post(auth_nonce))
-        .route(Path::Session.as_str(), post(auth_session))
+        .route(AuthPath::Nonce.as_str(), post(auth_nonce))
+        .route(AuthPath::Session.as_str(), post(auth_session))
         .route(
-            Path::DeleteSession.as_str(),
+            AuthPath::DeleteSession.as_str(),
             delete(auth_delete_session)
                 .layer(middleware::from_fn_with_state(app_state, authenticate_user)),
         )
