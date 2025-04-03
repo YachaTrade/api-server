4b967f3bcef8b1eebc5105fd0bc13ecab0b7d01d
gyu <gyu@gyuui-MacBookPro.local>
2025-04-03 15:10:33 +0900

    Fix : order token latest_king optimized

--- src/types/token/order.rs
+++ src/types/token/order.rs
@@ -162,7 +162,7 @@ impl OrderController {
             TokenOrderType::LatestTrade => {
                 sqlx::query_as::<_, OrderTokenRaw>(
                     r#"
-                                SELECT 
+                    SELECT 
                     t.token_id, a.account_id, a.nickname, a.image_uri as account_image_uri,
                     a.follower_count, a.following_count, t.name, t.symbol,
                     t.image_uri as token_image_uri, t.description,
@@ -232,18 +232,13 @@ impl OrderController {
     pub async fn get_latest_king_of_the_hill(&self) -> Result<Option<OrderToken>> {
         let row = sqlx::query_as::<_, OrderTokenRaw>(
             r#"
-            WITH latest_king AS (
-                SELECT token_id, created_at
-                FROM king
-                WHERE created_at = (SELECT MAX(created_at) FROM king)
-            )
             SELECT 
                 t.token_id,
                 a.account_id,
                 a.nickname,
                 a.image_uri as account_image_uri,
-                a.follower_count as follower_count,
-                a.following_count as following_count,
+                a.follower_count,
+                a.following_count,
                 t.name,
                 t.symbol,
                 t.image_uri as token_image_uri,
@@ -251,16 +246,17 @@ impl OrderController {
                 COALESCE(trc.reply_count::TEXT, '0') as reply_count,
                 COALESCE(m.price::TEXT, '0') as price,
                 COALESCE(m.reserve_token, '0') as reserve_token,
-                COALESCE(lk.token_id IS NOT NULL, false) as is_king,
-                lk.created_at as is_king_created_at,
+                TRUE as is_king,
+                k.created_at as is_king_created_at,
                 m.market_type,
-                t.created_at as created_at,
-                COALESCE(lk.created_at::FLOAT8, 0) as score
-            FROM latest_king lk
-            JOIN token t ON t.token_id = lk.token_id
+                t.created_at,
+                k.created_at::FLOAT8 as score
+            FROM king k
+            JOIN token t ON t.token_id = k.token_id
             JOIN account a ON t.creator = a.account_id
             LEFT JOIN token_reply_count trc ON t.token_id = trc.token_id
             LEFT JOIN market m ON t.token_id = m.token_id
+            WHERE k.created_at = (SELECT MAX(created_at) FROM king)
             "#,
         )
         .fetch_optional(self.db.get_read_pool())
