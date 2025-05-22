5c92fabaf10435375c75272a900cd5900fde795e
gyu <gyu@gyuui-MacBookPro.local>
2025-05-22 14:34:41 +0900

    Fix : pagination direction

--- src/types/common/pagination.rs
+++ src/types/common/pagination.rs
@@ -7,6 +7,8 @@ pub struct PaginationParams {
     pub page: i64,
     #[serde(default = "default_limit", deserialize_with = "validate_limit")]
     pub limit: i64,
+    #[serde(default = "default_direction", deserialize_with = "validate_direction")]
+    pub direction: String,
 }
 
 impl PaginationParams {
@@ -16,12 +18,31 @@ impl PaginationParams {
     }
 }
 
+fn default_direction() -> String {
+    "DESC".to_string()
+}
+
 fn default_page() -> i64 {
     1
 }
 
 fn default_limit() -> i64 {
     10
+}
+
+fn validate_direction<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let direction = String::deserialize(deserializer)?;
+    let direction_upper = direction.to_uppercase();
+    if direction_upper != "DESC" && direction_upper != "ASC" {
+        Err(serde::de::Error::custom(
+            "Direction must be either DESC or ASC",
+        ))
+    } else {
+        Ok(direction_upper)
+    }
 }
 
 fn validate_limit<'de, D>(deserializer: D) -> Result<i64, D::Error>
