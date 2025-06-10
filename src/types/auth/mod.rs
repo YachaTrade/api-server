55d79be3670fdb5a8b8198c66420d9a52ecf7d25
gyu <gyu@gyuui-MacBookPro.local>
2025-06-10 15:39:55 +0900

    Fix : nonce sign message

--- src/types/auth/mod.rs
+++ src/types/auth/mod.rs
@@ -18,16 +18,13 @@ pub struct AuthNonceRequest {
 
 #[derive(Debug, Serialize, ToSchema)]
 #[schema(example = json!({
-    "nonce": "abced-abced-abced",
-    "message": "example.com wants you to sign in with your Ethereum account:\n0x0000000000000000000000000000000000000000\n\nURI: https://example.com/login\nVersion: 1\nChain ID: 1\nNonce: abced-abced-abced\nIssued At: 2023-01-01T00:00:00Z"
+    "nonce": "example.com wants you to sign in with your Ethereum account:\n0x0000000000000000000000000000000000000000\n\nURI: https://example.com/login\nVersion: 1\nChain ID: 1\nNonce: abced-abced-abced\nIssued At: 2023-01-01T00:00:00Z"
 }))]
 pub struct AuthNonceResponse {
-    #[schema(example = "abced-abced-abced")]
-    pub nonce: String,
     #[schema(
         example = "example.com wants you to sign in with your Ethereum account:\n0x0000000000000000000000000000000000000000\n\nURI: https://example.com/login\nVersion: 1\nChain ID: 1\nNonce: abced-abced-abced\nIssued At: 2023-01-01T00:00:00Z"
     )]
-    pub message: String,
+    pub nonce: String,
 }
 
 #[derive(Debug, Deserialize, ToSchema)]
