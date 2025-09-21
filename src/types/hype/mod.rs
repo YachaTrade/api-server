3290b90978d3a31b222a60578fc85e50a5adec0d
gyu <gyu@gyuui-MacBookPro.local>
2025-09-21 23:20:58 +0900

    Fix : hype token query error

--- src/types/hype/mod.rs
+++ src/types/hype/mod.rs
@@ -24,12 +24,10 @@ use crate::{
     utils::single_flight::{GLOBAL_CACHE, with_cache},
 };
 
-
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct HypeTokenQuery {
-    epoch: Option<i64>,
+    pub epoch: Option<i64>,
 }
-
 
 // 데이터베이스 쿼리 결과를 담을 구조체
 #[derive(Debug, sqlx::FromRow)]
