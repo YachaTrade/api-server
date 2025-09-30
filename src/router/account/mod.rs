20c6a8bb84b8f569246b7bcb44861c21b05ec69c
gyu <gyu@gyuui-MacBookPro.local>
2025-09-30 21:36:10 +0900

    Fix : disconnect x

--- src/router/account/mod.rs
+++ src/router/account/mod.rs
@@ -2,7 +2,7 @@ pub mod handler;
 pub mod path;
 use axum::{
     Router,
-    routing::{get, patch, put},
+    routing::{delete, get, patch, put},
 };
 
 use path::AccountPath;
@@ -17,10 +17,10 @@ pub fn router() -> Router<AppState> {
         )
         .route(AccountPath::GetAccount.as_str(), get(handler::get_account))
         .route(AccountPath::ConnectX.as_str(), put(handler::connect_x))
-        // .route(
-        //     AccountPath::DisconnectX.as_str(),
-        //     delete(handler::disconnect_x),
-        // )
+        .route(
+            AccountPath::DisconnectX.as_str(),
+            delete(handler::disconnect_x),
+        )
         .route(AccountPath::UpdateX.as_str(), patch(handler::update_x))
         .route(
             AccountPath::RegisterWallet.as_str(),
